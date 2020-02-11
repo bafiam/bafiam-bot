6b4cebace74e904060404720d0fd9d569ab76d08
bafiam <stevebafiam@gmail.com>
2020-02-11 15:54:19 +0300

    add user class to fetch user data from slack api, process data and return name and email;set up the command;factor in token from enviroment

--- app/commands/bot.rb
+++ app/commands/bot.rb
@@ -1,8 +1,42 @@
 # frozen_string_literal: true
 
+require 'rest-client'
+require 'json'
+
 module Bafiambot
   module Commands
+    # get user info bot
     class GetUserInfo < SlackRubyBot::Commands::Base
+      command 'get_info' do |client, data, _match|
+        output = Users.fetch_data
+        send = Users.get_all_users_name(output)
+        client.say(channel: data.channel, text: send)
+      end
     end
+  end
+end
+
+# get user info
+class Users
+  def self.fetch_data
+    get_token = ENV['SLACK_API_TOKEN']
+    url = "https://slack.com/api/users.list?token=#{get_token}"
+    response = RestClient.get(url)
+    output = JSON.parse(response)
+    output
+  end
+
+  def self.get_all_users_name(data)
+    members = data['members']
+    i = 0
+    user_names = []
+    while i < members.length
+      username = members[i]['name']
+      email = members[i]['profile']['email']
+      user_names.push("#{username} : #{email}") unless email.nil?
+      i += 1
+    end
+
+    user_names
   end
 end
--- main.rb
+++ main.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'slack-ruby-bot'
 require './app/app.rb'
 require './app/commands/bot.rb'
