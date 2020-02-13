d4d780b6184021d7ce131fa6c3af3c556ec21331
bafiam <stevebafiam@gmail.com>
2020-02-14 02:50:40 +0300

    correct isssues in the dummy data files

--- spec/quote_data.rb
+++ spec/quote_data.rb
@@ -1,7 +1,7 @@
-# rubocop:disable LineLength
+# rubocop:disable Layout/LineLength
 class Testdata
   def self.run_data
     '{"author":"Keith Bostic","id":18,"quote":"Perl \u2013 The only language that looks the same before and after RSA encryption.","permalink":"http://quotes.stormconsultancy.co.uk/quotes/18"}'
   end
 end
-# rubocop:enable LineLength
+# rubocop:enable Layout/LineLength
